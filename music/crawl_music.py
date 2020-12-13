bd581c78212a8a96e2bd7e461fc473f90360a675
wfs2010 <1337581543@qq.com>
2020-12-13 13:42:35 +0800

    music

--- music/crawl_music.py
+++ music/crawl_music.py
@@ -4,3 +4,10 @@
 # @FILE     : crawl_music.py
 # @Time     : 2020-12-13 下午 1:40
 # @Software : PyCharm
+
+import requests
+import json
+
+url ='https://api.i-meto.com/meting/api?server=netease&type=playlist&id=2142580576&r=0.8248415771905837'
+
+html = requests.get('')
